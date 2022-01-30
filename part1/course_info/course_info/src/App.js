f0de25eb424954c99b5b8bc9834fb2fcfd555f39
Pablo <alu0101225364@ull.edu.es>
2022-01-30 12:42:42 +0000

    Excercise 1.5

--- part1/course_info/course_info/src/App.js
+++ part1/course_info/course_info/src/App.js
@@ -4,32 +4,35 @@ import Header from './Header'
 import Total from './Total'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      excercises: 10
-    } ,
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        excercises: 10
+      } ,
 
-    {
-      name: 'Using props to pass data',
-      excercises: 7
-    } ,
+      {
+        name: 'Using props to pass data',
+        excercises: 7
+      } ,
 
-    {
-      name: 'State of a component',
-      excercises: 14
-    } 
-  ]
+      {
+        name: 'State of a component',
+        excercises: 14
+      } 
+    ]
+  }
 
 
+  console.log(course["parts"][0])
 
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts}/>
+      <Content parts={course['parts']} />
+      <Total parts={course['parts']}/>
 </div>
   )
 }
