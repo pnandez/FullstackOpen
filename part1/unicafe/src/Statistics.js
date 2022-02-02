c697edae8ca7557eaaa5aa07751522aa37e400b0
Pablo <alu0101225364@ull.edu.es>
2022-02-02 11:39:15 +0000

    Excercise 1.7

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,4 +1,11 @@
 const Statistics = (props) =>{
+
+  const totalFeedback = props.statisticsDict.good + props.statisticsDict.bad + props.statisticsDict.neutral
+  
+  const averageFeedback = totalFeedback === 0 ? 0 :(props.statisticsDict.good - props.statisticsDict.bad) / totalFeedback
+
+  const positiveFeedbackPercentage = totalFeedback === 0 ? 0 :(props.statisticsDict.good/ totalFeedback) *100
+
   return (
     <div>
       <h2>Statistics</h2>
@@ -7,6 +14,12 @@ const Statistics = (props) =>{
         Neutral {props.statisticsDict.neutral}
         <br />
         Bad {props.statisticsDict.bad}
+        <br />
+        Total {totalFeedback}
+        <br />
+        Average {averageFeedback}
+        <br />
+        Positive {positiveFeedbackPercentage} %
     </div>
   )
 }
