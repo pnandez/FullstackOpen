0cc661746f438db5e178bdd66aeba79e372979e7
Pablo <alu0101225364@ull.edu.es>
2022-02-04 12:32:25 +0000

    Excercises 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import DisplayPerson from './Components/DisplayPerson'
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNewNameToPhoneBook = (event) =>{
+    event.preventDefault()
+    
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewNameToPhoneBook}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          debug: {newName}
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <DisplayPerson key={person['name']} person={person} />)}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/Components/DisplayPerson.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DisplayPerson = ({person}) =>{
+  return(
+    <p>
+      {person['name']}
+      </p>
+  )
+}
+
+export default DisplayPerson
