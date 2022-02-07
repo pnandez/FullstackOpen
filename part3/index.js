f2990325e5eb247868763085e3b034e91ba2b8a3
Pablo <alu0101225364@ull.edu.es>
2022-02-07 13:50:57 +0000

    Excercise 3.5 && 3.6

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,7 @@
 const { request, response } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let phonebook = [
     { 
@@ -49,9 +50,45 @@ app.delete('/persons/:id', (request,response) => {
   response.status(204).end()
 })
 
+app.post('/persons', (request,response) => {
+  const body= request.body
+
+  if(!body.name || !body.number){
+    return response.status(400).json({
+      error: "Name or number missing"
+    })
+  }
+  if(phonebook.find(person => person.name === body.name)){
+    return response.status(400).json({
+      error: "Person already exists in phonebook"
+    })
+  }
+  console.log(body);
+
+  const person = {
+    "name": body.name,
+    "number": body.number,
+    "id": generateID()
+  }
+
+  phonebook = phonebook.concat(person)
+
+  response.json(phonebook)
+})
+
+const generateID = () =>{
+  let id = 0
+  do{
+    id =  Math.floor(Math.random()*200)
+  }while(phonebook.find(person => person.id ===id))
+  return id
+}
+
 app.get('/info', (request, response) => {
   response.send(`<div> <p>Phonebook has info for ${phonebook.length} people</p> <p>${new Date()}</p> </div>`)
 })
+
+
 
 
 const PORT = 3001
