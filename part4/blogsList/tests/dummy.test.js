5ca358938cd54130cb98f0169a6c559ddb2098ed
Pablo <alu0101225364@ull.edu.es>
2022-02-14 14:18:35 +0000

    Excercise 4.4

--- part4/blogsList/tests/dummy.test.js
+++
@@ -1,9 +0,0 @@
-const dummy = require('../utils/list_helpers').dummy
-
-test('dummy returns 1', () =>{
-  const blogs = []
-
-  const result = dummy(blogs)
-
-  expect(result).toBe(1)
-})
--- part4/blogsList/utils/list_helpers.js
+++ part4/blogsList/utils/list_helpers.js
@@ -2,7 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if(blogs.length === 0) return 0 
+  if(blogs.length === 1) return blogs[0].likes
+
+  const result = blogs.reduce((a,b) => a + b.likes,0)
+  console.log(result);
+
+  return result
+}
+
 
 module.exports = {
-  dummy,
+  dummy, totalLikes
 }
